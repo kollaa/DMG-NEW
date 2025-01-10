c454b7b75b6daea644702b7fc1701520cfcb4bd3
Akhil Kolla <akolla@dynastymgmtgroup.com>
2025-01-10 11:33:20 -0600

    Initial commit with folders

---
+++ Backend/Printfx/src/main/java/com/dmg/spring/Printfx/PrintfxApplication.java
@@ -0,0 +1,13 @@
+package com.dmg.spring.Printfx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrintfxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrintfxApplication.class, args);
+	}
+
+}
---
+++ Backend/Printfx/src/test/java/com/dmg/spring/Printfx/PrintfxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dmg.spring.Printfx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrintfxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
